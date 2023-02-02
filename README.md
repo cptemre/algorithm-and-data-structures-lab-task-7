c75c0a679479d9d7df00f4999a230f84d5c1fc1c
cptemre <kunduraci2@gmail.com>
2023-02-02 12:36:53 +0100

    Project Upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# ALGORITHMS AND DATA STRUCTURES LABORATORY
+
+## TASK 7
+
+### GENERAL INFORMATION
+
+1. This project has been prepared for a course in informatics in the first semester.
+
+2. Project is prepared in Visual Studio 2022 application in Windows 11 OS. It may not work as intended in other platforms.
+
+3. Project only contains the code in github not the project folder prepared by Visual Studio.
+
+### DESCRIPTION OF THE TASK
+
+Save into two-dimensional array of size m,n-elements,values provided by the user
+Display:
+a) content of array,
+b) the sum of all elements in a array
+c) element [m,n] selected by the user.
---
+++ task-7.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    /*DECLARE THE VARIABLES AND ARRAY SIZE*/
+    int m, n, answerC, selectedRow, selectedColumn = 0;
+    int answerB = 0;
+
+    cout << "Please enter the row size: ";
+    cin >> m;
+    cout << endl;
+    cout << "Please enter the column size: ";
+    cin >> n;
+    vector<vector<int>> myList(m, vector<int>(n));
+
+    /*CREATE THE ITEMS*/
+    for (int i = 0; i < m; i++)
+    {   
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Please enter the value of row " << i << " column " << j << ": ";
+            cin >> myList[i][j];
+        }
+    }
+
+
+    /*LOOP TO SHOW THE 2D VECTOR AND SUM IT FOR ANSWER A AND B*/
+    cout << "\na-Content of Array:\n";
+    for (int i = 0; i < m; i++)
+    {
+        if (i == 0)
+        {
+            for (int z = 0; z < n; z++)
+            {
+                cout << "\tCOLUMN " << z;
+            }
+            cout << "\n\nROW " << i;
+        }
+        else {
+            cout << "\nROW " << i;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            cout << "\t\t" << myList[i][j];
+            /*SUM ANSWER B*/
+            answerB += myList[i][j];
+        }
+    }
+
+    cout << "\n\nb-The sum of all elements in the array: " << answerB << endl;
+
+    /*GET THE REQUESTED VALUE FROM THE VECTOR*/
+    cout << "Please enter the row and column position of the value you want to see.\n";
+    cout << "Selected row: ";
+    cin >> selectedRow;
+    cout << "\nSelected column: ";
+    cin >> selectedColumn;
+    answerC = myList[selectedRow][selectedColumn];
+    cout << "\nc-Element [" << selectedRow << " , " << selectedColumn << "] : " << answerC;
+
+    return 0;
+}
